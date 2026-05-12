fef36c91c9a155469b60692fb23f94cd39c271f4
suntipapm781-gif <suntipapm781@gmail.com>
2026-05-13 04:31:47 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,56 +3,56 @@ export default function Home() {
     <main className="w-full bg-black text-white">
 
       {/* HERO IMAGE */}
-      <section className="w-full min-h-screen flex items-center">
+      <section className="w-full overflow-hidden">
         <img
           src="/hero.png"
           alt="KHDESIGN Collection"
-          className="w-full h-auto object-contain select-none"
+          className="w-full h-auto block select-none"
         />
       </section>
 
       {/* BG1 */}
-      <section className="w-full h-screen flex justify-center items-center">
+      <section className="w-full">
         <img
           src="/bg1.png"
           alt="bg1"
-          className="w-full h-auto object-contain select-none"
+          className="w-full h-auto block select-none"
         />
       </section>
 
       {/* BG2 */}
-      <section className="w-full h-screen flex justify-center items-center">
+      <section className="w-full">
         <img
           src="/bg2.png"
           alt="bg2"
-          className="w-full h-auto object-contain select-none"
+          className="w-full h-auto block select-none"
         />
       </section>
 
       {/* BG3 */}
-      <section className="w-full h-screen flex justify-center items-center">
+      <section className="w-full">
         <img
           src="/bg3.png"
           alt="bg3"
-          className="w-full h-auto object-contain select-none"
+          className="w-full h-auto block select-none"
         />
       </section>
 
       {/* BG4 */}
-      <section className="w-full h-screen flex justify-center items-center">
+      <section className="w-full">
         <img
           src="/bg4.png"
           alt="bg4"
-          className="w-full h-auto object-contain select-none"
+          className="w-full h-auto block select-none"
         />
       </section>
 
       {/* BG5 */}
-      <section className="w-full h-screen flex justify-center items-center">
+      <section className="w-full">
         <img
           src="/bg5.png"
           alt="bg5"
-          className="w-full h-auto object-contain select-none"
+          className="w-full h-auto block select-none"
         />
       </section>
 
