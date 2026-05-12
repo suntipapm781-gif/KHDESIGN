b43b6668e2ca8c2e197de25c54508f6248f13424
suntipapm781-gif <suntipapm781@gmail.com>
2026-05-13 05:12:21 +0700

    Update page.js

--- src/app/shirt-print/page.js
+++ src/app/shirt-print/page.js
@@ -3,7 +3,7 @@ export default function ShirtPrint() {
     <main className="w-full bg-black">
       <section className="w-full overflow-hidden">
         <img
-          src="/hero.png"
+          src="/bg2.png"
           alt="เสื้อพิมพ์ลาย"
           className="w-full h-auto block select-none"
         />
